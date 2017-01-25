48f012fd2d80c6feaf4dda7c3bb95d25f6fbe45a
Andor Kelemen <andor.kelemen@gmail.com>
2017-01-25 21:29:12 +0100

    Add adele-application maven module (#3)

---
+++ adele-application/src/main/java/com/microservicesteam/adele/AdeleApplication.java
@@ -0,0 +1,11 @@
+package com.microservicesteam.adele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdeleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdeleApplication.class, args);
+    }
+}
