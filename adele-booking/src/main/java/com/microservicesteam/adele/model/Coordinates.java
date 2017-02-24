8c7d3d778aa5de6ead7423d43a48a1c72e58f771
kelemena <andor.kelemen@gmail.com>
2017-02-24 22:08:27 +0100

    CoordinatesDo

--- adele-booking/src/main/java/com/microservicesteam/adele/model/Coordinates.java
+++ adele-booking/src/main/java/com/microservicesteam/adele/model/Coordinates.java
@@ -4,9 +4,16 @@ import org.immutables.value.Value;
 
 @Value.Immutable
 public interface Coordinates {
-	
-	double latitude();
-	
-	double longitude();
+
+    double latitude();
+
+    double longitude();
+
+    class Builder extends ImmutableCoordinates.Builder {
+    }
+
+    static Builder builder() {
+        return new Builder();
+    }
 
 }
