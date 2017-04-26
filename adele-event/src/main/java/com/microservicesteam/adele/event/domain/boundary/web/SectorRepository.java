c871f99341b10986d9c89d32f561cdcef3e19eaf
kelemena <andor.kelemen@gmail.com>
2017-04-26 19:29:13 +0200

    Delete extra domain folder

--- adele-event/src/main/java/com/microservicesteam/adele/event/domain/boundary/web/SectorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservicesteam.adele.event.domain.boundary.web;
-
-import com.microservicesteam.adele.event.domain.domain.data.SectorDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "sectors", path = "sectors")
-public interface SectorRepository extends JpaRepository<SectorDo, Long> {
-}
--- adele-event/src/main/java/com/microservicesteam/adele/event/domain/domain/EventStatus.java
+++
@@ -1,8 +0,0 @@
-package com.microservicesteam.adele.event.domain.domain;
-
-public enum EventStatus {
-
-    OPEN,
-    CLOSED;
-    
-}
