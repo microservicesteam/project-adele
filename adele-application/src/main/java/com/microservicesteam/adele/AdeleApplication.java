f753520062df1414ded106c7adb94a0811431ceb
bearcheese <bearcheese@gmail.com>
2017-05-06 11:51:08 +0200

    Added websocket support (#13)
    * Added dummy websocket support
    
    * Added scheduler bits for websocket demo
    
    * Final pieces of websocket demo
    
    * Fixed review comments
    
    * Retrofit changes from master

--- adele-application/src/main/java/com/microservicesteam/adele/AdeleApplication.java
+++ adele-application/src/main/java/com/microservicesteam/adele/AdeleApplication.java
@@ -2,8 +2,10 @@ package com.microservicesteam.adele;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.microservicesteam")
+@EnableScheduling
 public class AdeleApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdeleApplication.class, args);
---
+++ adele-booking/src/main/resources/static/app.js
@@ -0,0 +1,45 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+}
+
+function connect() {
+    var socket = new WebSocket("ws://localhost:8080/ws");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/tickets', function (ticketEvent) {
+            showEvent(JSON.parse(ticketEvent.body));
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function showEvent(event) {
+    $("#conversation").append("<tr><td>" + event.bookingId + "</td><td>" + event.positions[0].eventId + "</td><td>" + event.positions[0].sectorId + "/" + event.positions[0].id + "</td></tr>");
+}
+
+$(function () {
+    $("#disconnect").prop("disabled", "true");
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#start" ).click(function() { $.post('/rs/api/triggerTicketsBooked/start') });
+    $( "#stop" ).click(function() { $.post('/rs/api/triggerTicketsBooked/stop') });
+});
+
