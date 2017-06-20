56960edd8a7648e14cf7847f77a26691024a774a
kelemena <andor.kelemen@gmail.com>
2017-06-20 20:13:56 +0200

    Polymorphic JSON serialization/deserialization on events

--- adele-ticket-master-api/src/main/java/com/microservicesteam/adele/ticketmaster/events/TicketsBooked.java
+++ adele-ticket-master-api/src/main/java/com/microservicesteam/adele/ticketmaster/events/TicketsBooked.java
@@ -2,7 +2,14 @@ package com.microservicesteam.adele.ticketmaster.events;
 
 import org.immutables.value.Value;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 @Value.Immutable
+@JsonSerialize(as = ImmutableTicketsBooked.class)
+@JsonDeserialize(as = ImmutableTicketsBooked.class)
+@JsonTypeName("TicketsBooked")
 public interface TicketsBooked extends TicketsEvent {
     String bookingId();
 
