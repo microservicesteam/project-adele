66c3f3c19c68ce554422880a9546ee1a752654a2
kelemena <andor.kelemen@gmail.com>
2017-07-04 20:17:50 +0200

    KAIZEN Avoid "*" import and re-optimize imports

--- adele-booking/src/main/java/com/microservicesteam/adele/booking/boundary/web/BookingController.java
+++ adele-booking/src/main/java/com/microservicesteam/adele/booking/boundary/web/BookingController.java
@@ -2,7 +2,12 @@ package com.microservicesteam.adele.booking.boundary.web;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.microservicesteam.adele.booking.domain.BookingRequest;
 import com.microservicesteam.adele.booking.domain.BookingResponse;
