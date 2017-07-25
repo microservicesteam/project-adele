0d83b43eea97ff32ef331c5a70dce931f720e97b
Peter Bodo <bodopeti89@gmail.com>
2017-07-25 20:50:57 +0200

    Logging added to adele-messaging module

--- adele-messaging/src/main/java/com/microservicesteam/adele/messaging/listeners/DeadEventListener.java
+++ adele-messaging/src/main/java/com/microservicesteam/adele/messaging/listeners/DeadEventListener.java
@@ -4,6 +4,8 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.google.common.eventbus.DeadEvent;
@@ -12,6 +14,8 @@ import com.google.common.eventbus.Subscribe;
 
 @Component
 public class DeadEventListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeadEventListener.class);
 
     private final EventBus eventBus;
     public final List<DeadEvent> deadEvents = new ArrayList<>();
@@ -22,6 +26,7 @@ public class DeadEventListener {
 
     @Subscribe
     public void handleEvent(DeadEvent event) {
+        LOGGER.debug("Dead event: {}", event);
         deadEvents.add(event);
     }
 
