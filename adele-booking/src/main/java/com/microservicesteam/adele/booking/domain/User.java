b7e3ec50d261db2454204959a8ac523025fba5b3
Peter Bodo <bodopeti89@gmail.com>
2017-10-03 19:41:33 +0200

    Getters eliminated and fields made public in package adele-style

--- adele-booking/src/main/java/com/microservicesteam/adele/booking/domain/User.java
+++ adele-booking/src/main/java/com/microservicesteam/adele/booking/domain/User.java
@@ -7,10 +7,14 @@ import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Value;
+import lombok.experimental.FieldDefaults;
 
 @Value
+@Getter(AccessLevel.NONE)
+@FieldDefaults(level = AccessLevel.PUBLIC)
 @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
--- adele-booking/src/main/java/com/microservicesteam/adele/booking/domain/Visitor.java
+++ adele-booking/src/main/java/com/microservicesteam/adele/booking/domain/Visitor.java
@@ -9,10 +9,14 @@ import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Value;
+import lombok.experimental.FieldDefaults;
 
 @Value
+@Getter(AccessLevel.NONE)
+@FieldDefaults(level = AccessLevel.PUBLIC)
 @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
