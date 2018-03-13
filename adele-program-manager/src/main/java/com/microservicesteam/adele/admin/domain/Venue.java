f552306ee3b9623edd7404ed72ab59f3edfce217
Andras Kantor <and.kantor@gmail.com>
2018-03-13 20:20:00 +0100

    KAIZEN renamed admin to program-manager

--- adele-program-manager/src/main/java/com/microservicesteam/adele/admin/domain/Venue.java
+++ adele-program-manager/src/main/java/com/microservicesteam/adele/admin/domain/Venue.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
 import java.util.List;
 
 import lombok.AccessLevel;
